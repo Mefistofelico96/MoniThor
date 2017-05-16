e4afcee21139e5da63ba7222d9584a7653b468d7
Cristina Salerno <cristina.salerno94@gmail.com>
2017-05-16 23:17:26 +0200

    Added some labels and missing backgrounds

--- Monithor/Monithor/ViewControllerDeviceDetails.swift
+++ Monithor/Monithor/ViewControllerDeviceDetails.swift
@@ -12,8 +12,7 @@ class ViewControllerDeviceDetails: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(red: 236/255, green: 254/255, blue: 240/255, alpha: 1.0)
     }
 
     override func didReceiveMemoryWarning() {
