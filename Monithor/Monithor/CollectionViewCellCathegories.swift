ce34c16dc6d4600d1c3f636880adf1debf28481b
Cristina Salerno <cristina.salerno94@gmail.com>
2017-05-17 15:30:19 +0200

    Added come tableview and collectionview cells labels and images

---
+++ Monithor/Monithor/CollectionViewCellCathegories.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCellCathegories.swift
+//  Monithor
+//
+//  Created by Cristina Salerno on 17/05/17.
+//  Copyright © 2017 Pipsqueaks. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCellCathegories: UICollectionViewCell {
+    
+}
---
+++ Monithor/Monithor/TableViewCellPowerStrip.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCellPowerStrip.swift
+//  Monithor
+//
+//  Created by Cristina Salerno on 17/05/17.
+//  Copyright © 2017 Pipsqueaks. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellPowerStrip: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
