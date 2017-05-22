f1994910a25534cbc08bfa4b0c6e66ff256118cb
Cristina Salerno <cristina.salerno94@gmail.com>
2017-05-22 14:56:32 +0200

    Merge branch 'master' of https://github.com/Mefistofelico96/PipsqueaksChallenge

---
+++ Monithor/Monithor/Cathegories.swift
@@ -0,0 +1,22 @@
+//
+//  Cathegories.swift
+//  Monithor
+//
+//  Created by Cristina Salerno on 22/05/17.
+//  Copyright © 2017 Pipsqueaks. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Cathegory{
+    var name: String?
+    var image: UIImage?
+    
+    init(name: String, image: UIImage) {
+        self.name = name
+        self.image = image
+    }
+}
+
+
